619eb63954ed4ded1d860cce2a185d76c7bd72ea
Erich Bieri <erich@bieris.net>
2023-01-22 11:54:32 +0100

    Added feather 32u4 pin config

--- config.h
+++ config.h
@@ -1,12 +1,18 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#define ARDUINO_EVOFW3_FEATHER_ATMEGA32U4
+
 #if defined ARDUINO_EVOFW3_ATMEGA32U4
 #include "atm32u4_pins.h"
 #define HOST_RATE 0
 
 #elif defined ARDUINO_EVOFW3_ATMEGA328P
 #include "atm328_pins.h"
+
+#elif defined ARDUINO_EVOFW3_FEATHER_ATMEGA32U4
+#include "atm32u4_pins.h"
+#define HOST_RATE 0
 
 #else
 #error "CC1101 connection not defined for target - use evofw3_avr board definitions"
@@ -21,6 +27,5 @@
 #else
 #error No Host baud rate defined
 #endif
-
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+
+
+extern "C" {
+  #include "evo.h"
+}
+
+void setup() {
+  main_init();
+}
+
+void loop() {
+  main_work();
+}
